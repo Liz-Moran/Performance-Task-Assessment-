8708527cb160d2861214394566e5e14d4acdf99a
Liz-Moran <lizmor0812@gmail.com>
2026-02-18 14:25:14 +0000

    Code 2/18

--- main.py
+++ main.py
@@ -15,25 +15,32 @@ print("Welcome to Mini Manager!")
 print("Where you'll be able to easily track your income and expenses!")
 print("Type 'stop' to stop entering your income and expenses.\n")
 
-while True:   #https://www.youtube.com/watch?v=n8NfyU5pX2U&t=249s 
+while True:   
     user_input = input("Enter amount (positive for income, negative for expenses) : ")
     if user_input.lower()=="stop":
         break
-    try:
+    try:                                      #https://www.youtube.com/watch?v=n8NfyU5pX2U&t=249s 
         amount = float(user_input)
     except:
         print("Not a number. Please try again.")
         continue
 
-    category= input("What is this money for?: ")
+    category= input("What is this/was money for?: ")
     add_transaction(amount, category)
 
     total = 0
     for t in transactions:
         total +=t[0]
 
-    print("Current balence: $", total, "(Category:", category + ")")
+    print("Current balance: $", total, "(Category:", category + ")")
     print("--------------------------")
 
-#Note to self: Add code to make the different categories and their amounts show up seprently 
+category_totals = {}
+for amount, category in transactions:
+    category_totals[category] = category_totals.get(category, 0)
++ amount
 
+print("\nCategory Summary")
+print("----------------")
+for category, total in category_totals.items():
+    print(f"{category}: ${total}")
